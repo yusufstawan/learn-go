d8206a65a8c115b6829e849ced70779915b0f8e4
yusufstawan <yusufsetiyawan45@gmail.com>
2022-08-11 20:48:10 +0700

    for range

--- sanbercode/Array-Slice-Map-Looping/main.go
+++ sanbercode/Array-Slice-Map-Looping/main.go
@@ -112,7 +112,41 @@ func main() {
 	// ======================= Looping =======================
 	// Perulangan Menggunakan Keyword for
 
-	for i := 0; i < 5; i++ {
-		fmt.Println("Angka", i)
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("Angka", i)
+	// }
+
+	// Penggunaan Keyword for Dengan Argumen Hanya Kondisi
+
+	// var i = 0
+	// for i < 5 {
+	// 	fmt.Println("Angka", i)
+	// 	i++
+	// }
+
+	// Penggunaan Keyword for Tanpa Argumen
+
+	// var i = 0
+
+	// for {
+	// 	fmt.Println("Angka", i)
+
+	// 	i++
+	// 	if i == 5 {
+	// 		break
+	// 	}
+	// }
+
+	// for range
+	// var fruits = [4]string{"apple", "grape", "banana", "melon"}
+
+	// for i, fruit := range fruits {
+	// 	fmt.Printf("elemen %d : %s\n", i, fruit)
+	// }
+
+	var fruits = [4]string{"apple", "grape", "banana", "melon"}
+
+	for _, fruit := range fruits {
+		fmt.Printf("nama buah : %s\n", fruit)
 	}
 }
